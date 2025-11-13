49901fa2ae3a213d04bdb4a0fbf2694c6f17da48
shav1x <alexo27112005@gmail.com>
2025-11-13 16:49:51 +0100

    Modified Person class

--- Project/Project/Classes/Person.cs
+++ Project/Project/Classes/Person.cs
@@ -5,8 +5,11 @@ namespace Project.Classes;
 public abstract class Person
 {
     public required string Name { get; set; }
+
     public required string Surname { get; set; }
+
     private string _email;
+
     public required string Email
     {
         get => _email;
@@ -18,9 +21,33 @@ public abstract class Person
             }
             _email = value;
         }
-
     }
+
     public string? Phone { get; set; }
-    public required DateTime BirthDate { get; set; }
+
+    private DateTime _dateOfBirth;
+
+    public required DateTime BirthDate
+    {
+        get => _dateOfBirth;
+        set
+        {
+            if (value > DateTime.Today)
+            {
+                throw new ArgumentException("Date of birth cannot be in the future", nameof(BirthDate));
+            }
+            _dateOfBirth = value;
+        }
+    }
+
     public int Age => DateTime.Today.Year - BirthDate.Year - (BirthDate > DateTime.Today.AddYears(-(DateTime.Today.Year - BirthDate.Year)) ? 1 : 0);
+
+    protected Person(string name, string surname, DateTime dateOfBirth, string email, string? phone)
+    {
+        _email = email;
+        _dateOfBirth = dateOfBirth;
+        Name = name;
+        Surname = surname;
+        Phone = phone;
+    }
 }
