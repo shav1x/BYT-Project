f854fcc39b685cd957ecec42bd46d5983da7476d
Milashevskyi Bohdan <s31717@pjwstk.edu.pl>
2025-11-13 16:39:55 +0100

    added class Customer

---
+++ Project/Project/Classes/Customer.cs
@@ -0,0 +1,14 @@
+﻿namespace Project.Classes;
+
+public class Customer : Person
+{
+    private static int _customerCount = 0;
+    public readonly int AccountId = _customerCount + 1;
+
+    public int BonusPoints { get; set; } = 0;
+
+    public Customer()
+    {
+        _customerCount++;
+    }
+}
