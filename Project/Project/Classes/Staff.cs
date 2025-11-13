b86cdd110676c9cbed01c104f13c600db655d364
mesh4ek <levanton38@gmail.com>
2025-11-13 16:52:53 +0100

    Added conctuctor to Staff

--- Project/Project/Classes/Staff.cs
+++ Project/Project/Classes/Staff.cs
@@ -1,7 +1,15 @@
 namespace Project.Classes;
 
-public class Staff : Person
+public class Staff(
+    string name,
+    string surname,
+    DateTime dateOfBirth,
+    string email,
+    string? phone,
+    string role,
+    decimal salary)
+    : Person(name, surname, dateOfBirth, email, phone)
 {
-    public required string Role { get; set; }
-    public required decimal Salary { get; set; }
+    public required string Role { get; set; } = role;
+    public required decimal Salary { get; set; } = salary;
 }
