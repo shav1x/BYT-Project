3210a56123fbb5d7db798ef1e8614492eda250bc
mesh4ek <levanton38@gmail.com>
2025-11-13 21:32:22 +0100

    Updated Staff

--- Project/Project/Classes/Staff.cs
+++ Project/Project/Classes/Staff.cs
@@ -2,8 +2,32 @@ namespace Project.Classes;
 
 public class Staff : Person
 {
-    public required string Role { get; set; }
-    public required decimal Salary { get; set; }
+    private decimal _salary;
+    private string? _role;
+    
+    public decimal Salary
+    {
+        get => _salary;
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException("Salary must be positive");
+
+            _salary = value;
+        }
+    }
+    
+    public string Role
+    {
+        get => _role;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Role cannot be empty");
+
+            _role = value;
+        }
+    }
     
     public Staff(string name, string surname, DateTime dateOfBirth, string email, string? phone, string role, decimal salary) : base(name, surname, dateOfBirth, email, phone)
     {
