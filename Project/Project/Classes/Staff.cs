9d8a2c5326d26ac5b2315a6e81ff366e6e71c9a1
mesh4ek <levanton38@gmail.com>
2025-11-13 16:59:54 +0100

    modified constructor in Staff

--- Project/Project/Classes/Staff.cs
+++ Project/Project/Classes/Staff.cs
@@ -1,15 +1,13 @@
 namespace Project.Classes;
 
-public class Staff(
-    string name,
-    string surname,
-    DateTime dateOfBirth,
-    string email,
-    string? phone,
-    string role,
-    decimal salary)
-    : Person(name, surname, dateOfBirth, email, phone)
+public class Staff : Person
 {
-    public required string Role { get; set; } = role;
-    public required decimal Salary { get; set; } = salary;
+    public required string Role { get; set; }
+    public required decimal Salary { get; set; }
+    
+    public Staff(string name, string surname, DateTime dateOfBirth, string email, string? phone, string role, decimal salary) : base(name, surname, dateOfBirth, email, phone)
+    {
+        Role = role;
+        Salary = salary;
+    }
 }
