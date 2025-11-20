1a793bafbbe27176e0a7af2bb1460ff5c632af84
shav1x <alexo27112005@gmail.com>
2025-11-20 16:27:13 +0100

    Added the extent to the staff class

--- Project/Project/Classes/Staff.cs
+++ Project/Project/Classes/Staff.cs
@@ -2,11 +2,12 @@ namespace Project.Classes;
 
 public class Staff : Person
 {
-    private static List<Staff> _staff = new List<Staff>();
-    
+    private static readonly List<Staff> _extent = new();
+    public static IReadOnlyList<Staff> Extent => _extent.AsReadOnly();
+
     private decimal _salary;
-    private string _role;
-    
+    private string _role = null!;
+
     public decimal Salary
     {
         get => _salary;
@@ -18,7 +19,7 @@ public class Staff : Person
             _salary = value;
         }
     }
-    
+
     public string Role
     {
         get => _role;
@@ -31,10 +32,33 @@ public class Staff : Person
         }
     }
     
-    public Staff(string name, string surname, DateTime dateOfBirth, string email, string? phone, string role, decimal salary) : base(name, surname, dateOfBirth, email, phone)
+    public Staff()
+    {
+    }
+
+    public Staff(
+        string name,
+        string surname,
+        DateTime birthDate,
+        string email,
+        string? phone,
+        string role,
+        decimal salary
+    ) : base(name, surname, birthDate, email, phone)
     {
         Role = role;
         Salary = salary;
-        _staff.Add(this);
+
+        _extent.Add(this);
+    }
+
+    public static void LoadExtent(List<Staff>? staffMembers)
+    {
+        _extent.Clear();
+
+        if (staffMembers is null || staffMembers.Count == 0)
+            return;
+
+        _extent.AddRange(staffMembers);
     }
 }
