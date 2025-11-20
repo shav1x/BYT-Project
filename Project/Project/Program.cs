f084e54b8e9c5948c4309603eb2c06c7b1a4522c
shav1x <alexo27112005@gmail.com>
2025-11-20 15:00:55 +0100

    Added static Lists for each class

--- Project/Project/Program.cs
+++ Project/Project/Program.cs
@@ -7,31 +7,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        try
-        {
-            Customer customer1 = new Customer(
-                name: "Andrii",
-                surname: "Doe",
-                dateOfBirth: new DateTime(1990, 5, 12),
-                email: "john.doe@example.com",
-                phone: "123456789"
-            );
-            Customer customer2 = new Customer(
-                name: "Bob",
-                surname: "Doe",
-                dateOfBirth: new DateTime(1990, 5, 12),
-                email: "john.doe@example.com",
-                phone: "123456789"
-            );
-            
-            Console.WriteLine(customer1.Name);
-            SerDes.SerializeToXml(customer1, "Cinema.xml");
-            SerDes.SerializeToXml(customer2, "Cinema.xml");
-            
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
+        
     }
 }
