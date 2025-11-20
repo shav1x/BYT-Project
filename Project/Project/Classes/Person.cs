89754674f35415abbb9967efc18eab6106bbadab
mesh4ek <levanton38@gmail.com>
2025-11-20 17:33:55 +0100

    Revert "little fix"
    This reverts commit ac381868617b0200eb80b373961ed01b8f1c0976.

--- Project/Project/Classes/Person.cs
+++ Project/Project/Classes/Person.cs
@@ -4,12 +4,12 @@ namespace Project.Classes;
 
 public abstract class Person
 {
-    private string _name = null!;
-    private string _surname = null!;
-    private string _email = null!;
+    private string _name;
+    private string _surname;
+    private string _email;
     private string? _phone;
     private DateTime _birthDate;
-
+    
     public int Age =>
         DateTime.Today.Year - BirthDate.Year -
         (BirthDate > DateTime.Today.AddYears(-(DateTime.Today.Year - BirthDate.Year)) ? 1 : 0);
@@ -24,6 +24,7 @@ public abstract class Person
             {
                 throw new ArgumentException("Name must contain only letters.");
             }
+
             _name = value;
         }
     }
@@ -38,6 +39,7 @@ public abstract class Person
             {
                 throw new ArgumentException("Surname must contain only letters.");
             }
+
             _surname = value;
         }
     }
@@ -52,6 +54,7 @@ public abstract class Person
             {
                 throw new ArgumentException("Invalid email format.");
             }
+
             _email = value;
         }
     }
@@ -61,10 +64,12 @@ public abstract class Person
         get => _phone;
         set
         {
-            if (value != null && !Regex.IsMatch(value, @"^[0-9]+$"))
+            if (value != null &&
+                !Regex.IsMatch(value, @"^[0-9]+$"))
             {
                 throw new ArgumentException("Phone must contain only numbers.");
             }
+
             _phone = value;
         }
     }
@@ -75,18 +80,21 @@ public abstract class Person
         set
         {
             if (value > DateTime.Today)
+            {
                 throw new ArgumentException("Birth date cannot be in the future.");
+            }
 
             _birthDate = value;
         }
     }
-
-    // For JSON serialization
-    protected Person() { }
+    
+    protected Person()
+    {
+    }
 
     protected Person(string name, string surname, DateTime birthDate, string email, string? phone)
     {
-        Name = name;
+        Name = name;          
         Surname = surname;
         BirthDate = birthDate;
         Email = email;
