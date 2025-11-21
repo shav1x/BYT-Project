ce806c444881b0c1bdaadfde0cedf1bc9529bf11
Milashevskyi Bohdan <s31717@pjwstk.edu.pl>
2025-11-21 13:56:05 +0100

    Added Seat class and unit tests for this class

--- Project/Project/Classes/Seat.cs
+++ Project/Project/Classes/Seat.cs
@@ -2,5 +2,53 @@ namespace Project.Classes;
 
 public class Seat
 {
+    public enum SeatType
+    {
+        VIP,
+        Standard,
+        Comfort
+    }
     
+    private int _number;
+    private int _row;
+    private SeatType _type;
+
+    public int Number
+    {
+        get => _number;
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException("Seat number must be greater than 0.");
+            _number = value;
+        }
+    }
+
+    public int Row
+    {
+        get => _row;
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException("Row must be greater than 0.");
+            _row = value;
+        }
+    }
+    public SeatType Type
+    {
+        get => _type;
+        set
+        {
+            if (!Enum.IsDefined(typeof(SeatType), value))
+                throw new ArgumentException("Invalid seat type specified.");
+            _type = value;
+        }
+    }
+    
+    public Seat(int number, int row, SeatType type)
+    {
+        Number = number;
+        Row = row;
+        Type = type;
+    }
 }
---
+++ Project/ProjectTests/SeatTests.cs
@@ -0,0 +1,77 @@
+﻿using Project.Classes;
+
+namespace ProjectTests;
+
+[TestFixture]
+public class SeatTests
+{
+    [Test]
+    public void Constructor_ValidData_CreatesSeat()
+    {
+        var seat = new Seat(5, 3, Seat.SeatType.VIP);
+
+        Assert.That(seat.Number, Is.EqualTo(5));
+        Assert.That(seat.Row, Is.EqualTo(3));
+        Assert.That(seat.Type, Is.EqualTo(Seat.SeatType.VIP));
+    }
+
+    [TestCase(1)]
+    [TestCase(10)]
+    [TestCase(99)]
+    public void SeatNumber_ValidNumber_True(int number)
+    {
+        var seat = new Seat(number, 3, Seat.SeatType.Standard);
+        Assert.That(seat.Number, Is.EqualTo(number));
+    }
+
+    [TestCase(0)]
+    [TestCase(-1)]
+    [TestCase(-50)]
+    public void SeatNumber_InvalidNumber_ThrowsArgumentException(int number)
+    {
+        Assert.That(
+            () => new Seat(number, 3, Seat.SeatType.Standard),
+            Throws.TypeOf<ArgumentException>()
+        );
+    }
+
+    [TestCase(1)]
+    [TestCase(5)]
+    [TestCase(20)]
+    public void SeatRow_ValidRow_True(int row)
+    {
+        var seat = new Seat(5, row, Seat.SeatType.Standard);
+        Assert.That(seat.Row, Is.EqualTo(row));
+    }
+
+    [TestCase(0)]
+    [TestCase(-1)]
+    [TestCase(-10)]
+    public void SeatRow_InvalidRow_ThrowsArgumentException(int row)
+    {
+        Assert.That(
+            () => new Seat(5, row, Seat.SeatType.Standard),
+            Throws.TypeOf<ArgumentException>()
+        );
+    }
+
+    [TestCase(Seat.SeatType.VIP)]
+    [TestCase(Seat.SeatType.Standard)]
+    [TestCase(Seat.SeatType.Comfort)]
+    public void SeatType_ValidType_True(Seat.SeatType type)
+    {
+        var seat = new Seat(5, 3, type);
+        Assert.That(seat.Type, Is.EqualTo(type));
+    }
+
+    [TestCase(-1)]
+    [TestCase(999)]
+    [TestCase(int.MaxValue)]
+    public void SeatType_InvalidType_ThrowsArgumentException(int typeValue)
+    {
+        Assert.That(
+            () => new Seat(5, 3, (Seat.SeatType)typeValue),
+            Throws.TypeOf<ArgumentException>()
+        );
+    }
+}
